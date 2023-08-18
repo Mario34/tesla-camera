b74dda961da0bd9b6b3bd329fbbe60b1bdb63d4d
Mario34 <zhl.mario34@outlook.com>
2023-08-18 10:14:32 +0800

    chore: 添加ffmpeg下载命令

--- init-binaries.sh
+++ init-binaries.sh
@@ -1,3 +1,7 @@
+curl -L https://github.com/Mario34/tesla-camera/releases/download/v1.0.0-pre/ffmpeg.zip >> ./src-tauri/binaries/ffmpeg.zip
+
+unzip -o ./src-tauri/binaries/ffmpeg.zip -d ./src-tauri/binaries/
+
 cp -f ./src-tauri/binaries/ffmpeg ./src-tauri/binaries/ffmpeg-aarch64-apple-darwin
 cp -f ./src-tauri/binaries/ffmpeg ./src-tauri/binaries/ffmpeg-x86_64-apple-darwin
 cp -f ./src-tauri/binaries/ffmpeg.exe ./src-tauri/binaries/ffmpeg-x86_64-pc-windows-msvc.exe
